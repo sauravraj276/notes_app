3f79709f4eae55434a891326a2f251fc9c72b0ca
Saurav Raj <sauravraj@gmail.com>
2023-03-26 21:49:33 +0530

    cors patch

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ const Note = require("./models/Note");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require("cors");
+app.use(cors());
 
 // app.get("/", function (req, res) {
 //   const response = { message: "API WORKS" };
