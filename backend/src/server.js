ff9a4c158d04da796b78020c75e0251b2c20f1bb
Saurav Raj <sauravraj@gmail.com>
2023-03-26 23:30:48 +0530

    Compiled flutter web buid added in public folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ const Note = require("./models/Note");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'))
 
 // app.get("/", function (req, res) {
 //   const response = { message: "API WORKS" };
