1b9badb5b826c2e3d070511c3049348c9a845314
Saurav Raj <sauravraj@gmail.com>
2023-03-27 09:31:15 +0530

    routes patch 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,28 +9,31 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.set("x-timestamp", Date.now());
+  res.set("x-powered-by", "cyclic.sh");
+  console.log(
+    `[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`
+  );
+  next();
+});
+
+var options = {
+  dotfiles: "ignore",
+  etag: false,
+  extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
+  index: ["index.html"],
+  maxAge: "1m",
+  redirect: false,
+};
+app.use(express.static("public", options));
+
 const cors = require("cors");
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
-});
+
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
-
-
 
 
 // app.get("/", function (req, res) {
@@ -54,7 +57,6 @@ const connectDB = async () => {
 
 //Routes go here
 
-
 // app.get("/", function (req, res) {
 //   const response = { message: "API WORKS" };
 //   res.json(response);
@@ -75,5 +77,3 @@ connectDB().then(() => {
     console.log("server started at PORT : " + PORT);
   });
 });
-
-
